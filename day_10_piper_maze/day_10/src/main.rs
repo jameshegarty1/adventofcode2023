21d88df0074097826b7e097fad15a9326f39958e
James Hegarty <jhegartyodowd@gmail.com>
2024-04-16 00:24:58 +0100

    [DAY 10] Few bits

--- day_10_piper_maze/day_10/src/main.rs
+++ day_10_piper_maze/day_10/src/main.rs
@@ -1,21 +1,40 @@
-fn parse_file(file_path: &str) -> Vec<Vec<i32>> {
+use std::fs;
+use std::cmp::Ordering;
+
+fn find_char_linear(target: char, data: &Vec<Vec<char>>) -> Option<(usize, usize)> {
+    for (i, row) in data.iter().enumerate() {
+        for (j, &ch) in row.iter().enumerate() {
+            if ch == target {
+                return Some((i,j))
+            }
+        } 
+    }
+    None
+}
+
+fn parse_file(file_path: &str) -> Vec<Vec<char>> {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let mut data_vec: Vec<Vec<i32>> = Vec::new();
+    let mut data_vec: Vec<Vec<char>> = Vec::new();
 
 
     contents.lines().for_each(|line| {
-        let items: Result<Vec<i32>,_> = line.split_whitespace().map(str::parse::<i32>).collect();
-        match items {
-            Ok(nums) => data_vec.push(nums),
-            Err(e) => println!("Err: {:?}", e),
-        }
+        let row: Vec<_> = line.chars().collect();
+        data_vec.push(row);
     });
 
     data_vec
 }
 
 fn main() {
-    let data: Vec<Vec<i32>> = parse_file("input_main");
+    let data: Vec<Vec<char>> = parse_file("test_input");
+
+    let target: char = 'S';
+    let start_location: (usize,usize) = match find_char_linear(target, &data) {
+        Some((i, j)) => (i, j),
+        None => (0,0)
+    };
+
+    let pointer_one
 }
