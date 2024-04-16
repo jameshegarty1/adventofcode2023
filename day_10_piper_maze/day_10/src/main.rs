a8e4ffd6c0999d496ba46e1daa88788d8b555944
James Hegarty <jhegartyodowd@gmail.com>
2024-04-16 09:22:20 +0100

    [DAY 10] Some data structures

--- day_10_piper_maze/day_10/src/main.rs
+++ day_10_piper_maze/day_10/src/main.rs
@@ -1,6 +1,55 @@
 use std::fs;
 use std::cmp::Ordering;
 
+enum Tile {
+    VerticalPipe,    // '|'
+    HorizontalPipe,  // '-'
+    BendNE,          // 'L'
+    BendNW,          // 'J'
+    BendSW,          // '7'
+    BendSE,          // 'F'
+    Ground,          // '.'
+    StartPosition,   // 'S'
+}
+
+impl Tile {
+   fn from_char(c: char) -> Option<Self> {
+        match c {
+            '|' => Some(Tile::VerticalPipe),
+            '-' => Some(Tile::HorizontalPipe),
+            'L' => Some(Tile::BendNE),
+            'J' => Some(Tile::BendNW),
+            '7' => Some(Tile::BendSW),
+            'F' => Some(Tile::BendSE),
+            '.' => Some(Tile::Ground),
+            'S' => Some(Tile::StartPosition),
+            _ => None, // Handle unexpected characters
+        }
+    }
+
+    fn connections(&self) -> &'static str {
+        match self {
+            Tile::VerticalPipe => "north-south",
+            Tile::HorizontalPipe => "east-west",
+            Tile::BendNE => "north-east",
+            Tile::BendNW => "north-west",
+            Tile::BendSW => "south-west",
+            Tile::BendSE => "south-east",
+            Tile::Ground => "none",
+            Tile::StartPosition => "unknown",
+        }
+    }     
+} 
+enum Directions {
+    North,
+    East,
+    South,
+    West
+}
+
+
+
+
 fn find_char_linear(target: char, data: &Vec<Vec<char>>) -> Option<(usize, usize)> {
     for (i, row) in data.iter().enumerate() {
         for (j, &ch) in row.iter().enumerate() {
@@ -36,5 +85,14 @@ fn main() {
         None => (0,0)
     };
 
-    let pointer_one
+    let first_connections = [start_location]
+
+    let pointer_one = start_location.clone();
+    let pointer_two = start_location.clone();
+
+    let completed_loop: bool = false;
+
+    while !completed_loop {
+        
+    }
 }
